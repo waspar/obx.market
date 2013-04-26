8da17488f779e870b9e1e4074ca9e834240054a3
pr0n1x <rootfavell@gmail.com>
2013-04-26 09:57:44 +0800

    Поправил скрипт сборки. Указывал на несуществующий файл

--- install/build.php
+++ install/build.php
@@ -1,20 +1,19 @@
 #!/usr/bin/env php
 <?php
-/***************************************
- ** @product OBX:Market Bitrix Module **
- ** @authors                          **
- **         Maksim S. Makarov         **
- **         Morozov P. Artem          **
- ** @License GPLv3                    **
- ** @mailto rootfavell@gmail.com      **
- ** @mailto tashiro@yandex.ru         **
- ***************************************/
+/***********************************************
+ ** @product OBX:Market Bitrix Module         **
+ ** @authors                                  **
+ **         Maksim S. Makarov aka pr0n1x      **
+ ** @License GPLv3                            **
+ ** @mailto rootfavell@gmail.com              **
+ ** @mailto tashiro@yandex.ru                 **
+ ** @copyright 2013 DevTop                    **
+ ***********************************************/
 
-require dirname(__FILE__)."/../../obx.core/classes/OBX_Build.php";
+require dirname(__FILE__)."/../../obx.core/classes/Build.php";
 $build = new OBX_Build("obx.market");
 $build->generateInstallCode();
 $build->generateUnInstallCode();
 $build->generateBackInstallCode();
 $build->backInstallResources();
 $build->generateMD5FilesList();
-?>

fcab8be4e9186fd2a9ac971ea1b04bf62682e9f0
pr0n1x <rootfavell@gmail.com>
2013-05-28 01:36:05 +0800

    ...

--- test/Util_RegisterModules.php
+++ test/Util_RegisterModules.php
@@ -17,5 +17,12 @@ class OBX_Test_Util_RegisterModules extends OBX_Market_TestCase {
 		}
 		$this->assertTrue(IsModuleInstalled('obx.core'));
 		$this->assertTrue(IsModuleInstalled('obx.market'));
+
+		require_once $_SERVER['DOCUMENT_ROOT'].BX_ROOT.'/modules/obx.core/install/index.php';
+		require_once $_SERVER['DOCUMENT_ROOT'].BX_ROOT.'/modules/obx.market/install/index.php';
+		$obx_core = new obx_core();
+		$obx_core->InstallEvents();
+		$obx_market = new obx_market();
+		$obx_market->InstallEvents();
 	}
 }

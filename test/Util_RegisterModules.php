c0547f03f374875b8c09e63f024ce231ac6936d8
pr0n1x <rootfavell@gmail.com>
2013-05-28 01:00:51 +0800

    Тест-утилита для регистрации модулей как установленных

---
+++ test/Util_RegisterModules.php
@@ -0,0 +1,21 @@
+<?php
+/***********************************************
+ ** @product OBX:Market Bitrix Module         **
+ ** @authors                                  **
+ **         Maksim S. Makarov aka pr0n1x      **
+ ** @License GPLv3                            **
+ ** @mailto rootfavell@gmail.com              **
+ ** @copyright 2013 DevTop                    **
+ ***********************************************/
+class OBX_Test_Util_RegisterModules extends OBX_Market_TestCase {
+	public function testRegisterModules(){
+		if( ! IsModuleInstalled('obx.core') ) {
+			RegisterModule('obx.core');
+		}
+		if( ! IsModuleInstalled('obx.market') ) {
+			RegisterModule('obx.market');
+		}
+		$this->assertTrue(IsModuleInstalled('obx.core'));
+		$this->assertTrue(IsModuleInstalled('obx.market'));
+	}
+}

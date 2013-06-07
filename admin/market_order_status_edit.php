180219281e7f58301ad7d3065041efc3a320809f
pr0n1x <rootfavell@gmail.com>
2013-06-08 01:37:30 +0800

    Копирайты под свободную лицензию

--- admin/market_order_status_edit.php
+++ admin/market_order_status_edit.php
@@ -1,13 +1,12 @@
 <?php
-/***************************************
- ** @product OBX:Market Bitrix Module **
- ** @authors                          **
- **         Maksim S. Makarov         **
- **         Morozov P. Artem          **
- ** @License GPLv3                    **
- ** @mailto rootfavell@gmail.com      **
- ** @mailto tashiro@yandex.ru         **
- ***************************************/
+/***********************************************
+ ** @product OBX:Market Bitrix Module         **
+ ** @authors                                  **
+ **         Maksim S. Makarov aka pr0n1x      **
+ ** @License GPLv3                            **
+ ** @mailto rootfavell@gmail.com              **
+ ** @copyright 2013 DevTop                    **
+ ***********************************************/
 
 use OBX\Market\CurrencyFormatDBS;
 use OBX\Market\Order;
--- install/build.php
+++ install/build.php
@@ -4,9 +4,8 @@
  ** @product OBX:Market Bitrix Module         **
  ** @authors                                  **
  **         Maksim S. Makarov aka pr0n1x      **
- ** @license GPLv3                            **
+ ** @License GPLv3                            **
  ** @mailto rootfavell@gmail.com              **
- ** @mailto tashiro@yandex.ru                 **
  ** @copyright 2013 DevTop                    **
  ***********************************************/
 

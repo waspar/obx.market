a9dff1279f8180fe94b3d3d16761d80b583c4c82
pr0n1x <rootfavell@gmail.com>
2013-06-19 03:43:50 +0800

    Обновлены файлы подмодуля obx.core. В предыдущем коммите Артем зачем-то удалил компонент

---
+++ install/components/obx.market/basket/.description.php
@@ -0,0 +1,24 @@
+<?php
+/***********************************************
+ ** @product OBX:Market Bitrix Module         **
+ ** @authors                                  **
+ **         Maksim S. Makarov aka pr0n1x      **
+ ** @license Affero GPLv3                     **
+ ** @mailto rootfavell@gmail.com              **
+ ** @copyright 2013 DevTop                    **
+ ***********************************************/
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED!==true) die();
+require $_SERVER['DOCUMENT_ROOT'].BX_ROOT.'/modules/obx.market/includes/'.LANGUAGE_ID.'/cmp_lang_desc.php';
+$arComponentDescription = array(
+	'NAME' => 'Basket',
+	'DESCRIPTION' => 'Basket',
+	'PATH' => array(
+		'ID' => 'obx_market',
+		'NAME' => GetMessage('OBX_MARKET_CMP_PATH_MARKET_NAME'),
+		'CHILD' => array(
+			'ID' => 'sale',
+			'NAME' => GetMessage('OBX_MARKET_BASKET_CMP_PATH_SALE'),
+			'SORT' => 10,
+		),
+	),
+);

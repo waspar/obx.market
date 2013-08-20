9b9f70d8bcfdd31fcca68abcfcd8f0e820197ba6
pr0n1x <rootfavell@gmail.com>
2013-08-21 03:18:43 +0800

    Добавлено событие подключения модуля

--- include.php
+++ include.php
@@ -4,6 +4,9 @@ IncludeModuleLangFile(__FILE__);
 if(!CModule::IncludeModule('iblock')){
     return false;
 }
+
+ExecuteModuleEventEx(GetModuleEvents('obx.market', 'onBeforeModuleInclude', true), array());
+
 if(!CModule::IncludeModule('obx.core')) {
 	$obxCorePath = $_SERVER['DOCUMENT_ROOT'].BX_ROOT.'/modules/obx.core/install/index.php';
 	if(!file_exists($obxCorePath) ) {
@@ -20,3 +23,8 @@ if(!CModule::IncludeModule('obx.core')) {
 $arModuleClasses = require dirname(__FILE__).'/classes/.classes.php';
 CModule::AddAutoloadClasses('obx.market', $arModuleClasses);
 require dirname(__FILE__).'/tools.php';
+$arEventList = GetModuleEvents('obx.market', 'onAfterModuleInclude', true);
+foreach($arEventList as $arEvent) {
+	ExecuteModuleEventEx($arEvent, array());
+}
+
